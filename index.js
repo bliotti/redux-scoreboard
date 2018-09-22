d0e05511b62f44f8c7abd8cbaa34aa225d89c538
Brian Liotti <bdliotti@gmail.com>
2018-09-22 12:13:25 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Scoreboard from './src/containers/Scoreboard'
+
+render(<Scoreboard />, document.getElementById('root'))
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { PropTypes } from 'react'
+import Stats from './Stats'
+import Stopwatch from './Stopwatch'
+
+const Header = props => (
+  <div className="header">
+    <Stats players={props.players} />
+    <h1>Scoreboard</h1>
+    <Stopwatch />
+  </div>
+)
+
+Header.propTypes = {
+  players: PropTypes.array.isRequired
+}
+
+export default Header
